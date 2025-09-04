4e135a37a6118c74e30526557a98535c139af4c5
Ethan <134173034+Aueza@users.noreply.github.com>
2025-09-04 19:19:06 +0000

    converting griffins c++ prog to c basic

--- cBasics.C
+++ cBasics.C
@@ -3,16 +3,38 @@
 #include <stdio.h>
 
 // Example of a struct.
-struct Student{
-    int studentID;
-    short year;
-    float gpa;
+struct Point{
+    int x;
+    int y;
 };
+
+/// @brief Constructor-like function to initialize a Point struct.
+/// @param p pointer to the Point struct to initialize.
+void initPoint(struct Point *p){
+    p->x = 0;
+    p->y = 0;
+}
+
+/// @brief Parameterized constructor-like function to initialize a Point struct.
+/// @param p pointer to the Point struct to initialize.
+/// @param x x value/coordinate.
+/// @param y y value/coordinate
+void buildPoint(struct Point *p, int x, int y){
+    p->x = x;
+    p->y = y;
+}
+
+/// @brief Function to print the values of a Point struct.
+/// @param p pointer to the Point struct to print.
+void printPoint(struct Point *p){
+    printf("Point(%d, %d)\n", p->x, p->y);
+}
+
 
 int main(){
     char name[50];
-    scanf("%c", &name);
-    printf("Hello, %c", name);
+    scanf("%s", &name);
+    printf("Hello, %s", name);
 
     return 0;
 }
